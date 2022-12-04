d0d0768d4dbeeec9ae42bb28acd54a0516335023
Piero <jhuayllahu@unsa.edu.pe>
2022-12-04 01:04:00 +0000

    se añadio una hoja de estilos y referencio a los .html

--- cgi-bin/script.pl
+++ cgi-bin/script.pl
@@ -12,19 +12,26 @@ print <<BLOCK;
 <!DOCTYPE html>
 <html lang="es">
   <head> 
-    <title></title>
+    <title>Caluladora</title>
     <meta charset="UTF-8">
-  </head>
+    <link rel="shortcurt icon" href="https://cdn-icons-png.flaticon.com/512/2374/2374370.png">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <link rel="stylesheet" href="../css/style.css"> 
+ </head>
 
-  <body>
-    <h1>Calculadora</h1>
-    <form action="../index.html">
-      <p>Calcular:</p>
-      <p>$expresion</p>
-      <p>Resultado:</p>
-      <p>$resultado</p>
-      <p><input type="submit" value="Regresar"></p>
-    </form>
+  <body class="main">
+    <div class="card">
+      <h1 class="header">Calculadora</h1>
+      <form action="../index.html">
+        <p class="texto">Calcular:</p>
+        <p class="texto resultado">$expresion</p>
+        <p class="texto">Resultado:</p>
+        <p class="texto resultado">$resultado</p>
+        <div class="botonContenedor">
+          <p><input class="boton" type="submit" value="Regresar"></p>
+        </div>
+      </form>
+    </div>
   </body>
 </html>
 BLOCK
@@ -32,7 +39,7 @@ BLOCK
 sub evaluarExpresion{
   my $expresion=$_[0];
 
-  while (not $expresion=~/^[0-9]+(\.[0-9]+)?$/){
+  while (not $expresion=~/^\-?[0-9]+(\.[0-9]+)?$/){
     if($expresion=~/No ingreso una expresion valida/){
       $expresion="No ingreso una expresion valida";
       last;
@@ -53,7 +60,7 @@ sub evaluarOperacion{
   
   if($expresion=~/\((.+)\)/){
     $resultado=evaluarExpresion($1);
-    if ($resultado=~/^[0-9]+(\.[0-9]+)?/){
+    if ($resultado=~/^\-?[0-9]+(\.[0-9]+)?$/){
       if ($expresion=~/([0-9]+(\.[0-9]+)?)\(.+\)/){
         $resultado=operacion($1,"*",$resultado);
         $expresion=~s/([0-9]+(\.[0-9]+)?)\(.+\)/($resultado)/;
@@ -74,9 +81,9 @@ sub evaluarOperacion{
     $resultado=operacion($1,$3,$4);
     $expresion=~s/([0-9]+(\.[0-9]+)?)([\*\/])([0-9]+(\.[0-9]+)?)/$resultado/;    
   } 
-  elsif ($expresion=~/([0-9]+(\.[0-9]+)?)([\+\-])([0-9]+(\.[0-9]+)?)/){
+  elsif ($expresion=~/(\-?[0-9]+(\.[0-9]+)?)([\+\-])([0-9]+(\.[0-9]+)?)/){
     $resultado=operacion($1,$3,$4);
-    $expresion=~s/([0-9]+(\.[0-9]+)?)([\+\-])([0-9]+(\.[0-9]+)?)/$resultado/;    
+    $expresion=~s/(\-?[0-9]+(\.[0-9]+)?)([\+\-])([0-9]+(\.[0-9]+)?)/$resultado/;    
   }
   else{
     $expresion="No ingreso una expresion valida";
